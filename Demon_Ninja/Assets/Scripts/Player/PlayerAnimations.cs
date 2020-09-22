c48ba9d98e6684b337a2a16811e08ee95a24d0e7
colorocker <colorocker@gmail.com>
2020-09-22 13:24:50 -0300

    Add PlayerMovement
    Add Player movement Running and Jump

---
+++ Demon_Ninja/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    PlayerController movement;    
+    Rigidbody2D rigidBody;
+    PlayerInput input;          
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<PlayerController>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        input = GetComponent<PlayerInput>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetBool("isRunning", movement.isRunning);
+        anim.SetBool("isJumping", movement.isJumping);
+        anim.SetBool("isAttacking", movement.isAttacking);
+    }
+}
