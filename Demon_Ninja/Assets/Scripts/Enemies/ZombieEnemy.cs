6924936e731f41e0a54088e120b0895b761c1fd2
Franco giovanardi <giovanardifranco1gmail.com>
2020-10-18 19:04:09 -0300

    Enemy>Zombie
    Zombie con movimiento entre varios puntos definidos.
    Animacion de movimiento.

---
+++ Demon_Ninja/Assets/Scripts/Enemies/ZombieEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieEnemy : MonoBehaviour
+{
+    public Transform[] waypoints;
+    
+    private float _moveSpeed = 2f;
+
+    private int _waypointIndex = 0;
+
+    private SpriteRenderer _zombieSprite;
+
+    void Start () {
+        transform.position = waypoints [_waypointIndex].transform.position;
+        _zombieSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update () {
+        Move ();
+    }
+
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards (transform.position,
+            waypoints[_waypointIndex].transform.position,
+            _moveSpeed * Time.deltaTime);
+
+        if (transform.position == waypoints [_waypointIndex].transform.position) {
+            _waypointIndex += 1;
+            Flip();
+        }
+				
+        if (_waypointIndex == waypoints.Length)
+            _waypointIndex = 0;
+    }
+
+    private void Flip()
+    {
+        _zombieSprite.flipX = !_zombieSprite.flipX;
+    }
+}
