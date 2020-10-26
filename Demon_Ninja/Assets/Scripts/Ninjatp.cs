f31b121ea7fad61089835de7aff7c0c7aec50645
Franco giovanardi <giovanardifranco1gmail.com>
2020-10-26 18:02:07 -0300

    level

---
+++ Demon_Ninja/Assets/Scripts/Ninjatp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninjatp : MonoBehaviour
+{
+    public Transform Destination;
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.position = Destination.transform.position;
+        }
+    }
+}
