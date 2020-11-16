44fda08185394a121dba70ab8072b3a4a204b06a
Franco giovanardi <giovanardifranco1gmail.com>
2020-11-15 21:18:06 -0300

    Cambio en camino principal

---
+++ Demon_Ninja/Assets/HiddenWall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiddenWall : Enemy
+{
+    private Collider2D thisCollider;
+    public GameObject showGameObject;
+    private Rigidbody2D thisRigid;
+    
+    void Start()
+    {
+        MaxHealth = 30;
+        CurrentHealth = 30;
+        thisCollider = GetComponent<BoxCollider2D>();
+        thisRigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsAlive)
+        {
+            thisCollider.enabled = false;
+            showGameObject.SetActive(false);
+        }
+    }
+    
+}
---
+++ Demon_Ninja/Assets/Scripts/JumpPlatform.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPlatform : MonoBehaviour
+{
+    public PlatformEffector2D _effector;
+
+    public float waitTime;
+    void Start()
+    {
+        _effector = GetComponent<PlatformEffector2D>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            waitTime = 0.5f;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _effector.rotationalOffset = 180f;
+                waitTime = 0.5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _effector.rotationalOffset = 0f;
+        }
+    }
+}
