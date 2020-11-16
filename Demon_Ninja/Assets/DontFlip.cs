77a7e3acd3244f854b3cd6d1a441af83f102905e
Franco giovanardi <giovanardifranco1gmail.com>
2020-11-16 18:49:35 -0300

    Press enter

---
+++ Demon_Ninja/Assets/DontFlip.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontFlip : MonoBehaviour
+{
+    public GameObject viking;
+
+    public SpriteRenderer thisRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        viking = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (viking.transform.localScale.x < 0)
+        {
+            thisRender.flipX = true;
+        }
+        if (viking.transform.localScale.x > 0)
+        {
+            thisRender.flipX = false;
+        }
+    }
+}
