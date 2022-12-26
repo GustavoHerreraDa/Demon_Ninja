e168f3fb4719735c019f68ef9ca98935b5974b27
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2022-12-26 20:38:44 -0300

    update menu scene fix selection

--- Demon_Ninja/Assets/Scripts/Menu/MenuManager.cs
+++ Demon_Ninja/Assets/Scripts/Menu/MenuManager.cs
@@ -24,27 +24,32 @@ public class MenuManager : MonoBehaviour
     }
     public void CheckMenuState()
     {
+
         switch (menuState)
         {
             case 1:
+                Debug.Log(menuState + " " + mainMenuCanvas.name);
                 mainMenuCanvas.enabled = true;
                 controlMenuCanvas.enabled = false;
                 creditsMenuCanvas.enabled = false;
                 playMenuCanvas.enabled = false;
                 break;
             case 2:
+                Debug.Log(menuState + " " + playMenuCanvas.name);
                 mainMenuCanvas.enabled = false;
                 controlMenuCanvas.enabled = false;
                 creditsMenuCanvas.enabled = false;
                 playMenuCanvas.enabled = true;
                 break;
             case 3:
+                Debug.Log(menuState + " " + controlMenuCanvas.name);
                 mainMenuCanvas.enabled = false;
                 controlMenuCanvas.enabled = true;
                 creditsMenuCanvas.enabled = false;
                 playMenuCanvas.enabled = false;
                 break;
             case 4:
+                Debug.Log(menuState + " " + creditsMenuCanvas.name);
                 mainMenuCanvas.enabled = false;
                 controlMenuCanvas.enabled = false;
                 creditsMenuCanvas.enabled = true;
